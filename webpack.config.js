aca5d2f4f19c5e4042b1932db79dd7cd927107f1
Timothy Sullivan <tsullivan@paypal.com>
2015-01-03 23:46:32 -0700

    fix indentation

--- webpack.config.js
+++ webpack.config.js
@@ -9,51 +9,51 @@ var webpack = require('webpack');
 
 module.exports = {
 
-  output: {
-    filename: 'main.js',
-    publicPath: '/assets/'
-  },
+	output: {
+		filename: 'main.js',
+		publicPath: '/assets/'
+	},
 
-  cache: true,
-  debug: true,
-  devtool: false,
-  entry: [
-      'webpack/hot/only-dev-server',
-      './src/scripts/components/main.jsx'
-  ],
+	cache: true,
+	debug: true,
+	devtool: false,
+	entry: [
+			'webpack/hot/only-dev-server',
+			'./src/scripts/components/main.jsx'
+	],
 
-  stats: {
-    colors: true,
-    reasons: true
-  },
+	stats: {
+		colors: true,
+		reasons: true
+	},
 
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  module: {
-    preLoaders: [{
-      test: '\\.js$',
-      exclude: 'node_modules',
-      loader: 'jshint'
-    }],
-    loaders: [{
-      test: /\.jsx$/,
-      loader: 'react-hot!jsx-loader?harmony'
-    }, {
-      test: /\.sass/,
-      loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded'
-    }, {
-      test: /\.css$/,
-      loader: 'style-loader!css-loader'
-    }, {
-      test: /\.(png|jpg)$/,
-      loader: 'url-loader?limit=8192'
-    }]
-  },
+	resolve: {
+		extensions: ['', '.js', '.jsx']
+	},
+	module: {
+		preLoaders: [{
+			test: '\\.js$',
+			exclude: 'node_modules',
+			loader: 'jshint'
+		}],
+		loaders: [{
+			test: /\.jsx$/,
+			loader: 'react-hot!jsx-loader?harmony'
+		}, {
+			test: /\.sass/,
+			loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded'
+		}, {
+			test: /\.css$/,
+			loader: 'style-loader!css-loader'
+		}, {
+			test: /\.(png|jpg)$/,
+			loader: 'url-loader?limit=8192'
+		}]
+	},
 
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ]
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoErrorsPlugin()
+	]
 
 };
