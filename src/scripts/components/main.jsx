75fd1c6f801bfa4f8be0a86a9f5b0f8759536d51
Timothy Sullivan <tsullivan@paypal.com>
2015-01-10 00:26:42 -0700

    copy example from react-router

--- src/scripts/components/main.jsx
+++ src/scripts/components/main.jsx
@@ -1,12 +1,43 @@
-/** @jsx React.DOM */
-
-var ReactWebpackApp = require('./ReactWebpackApp');
 var React = require('react');
-var {DefaultRoute, Route, Routes} = require('react-router');
+var Router = require('react-router');
+var { Route, RouteHandler, Link } = Router;
 
-React.renderComponent((
-  <Routes location="history">
-    <Route path="/" handler={ReactWebpackApp}>
-    </Route>
-  </Routes>
-), document.getElementById('content'));
+var App = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <ul>
+          <li><Link to="user" params={{userID: "123"}}>Bob</Link></li>
+          <li><Link to="user" params={{userID: "123"}} query={{showAge: true}}>Bob With Query Params</Link></li>
+          <li><Link to="user" params={{userID: "abc"}}>Sally</Link></li>
+        </ul>
+        <RouteHandler/>
+      </div>
+    );
+  }
+});
+
+var User = React.createClass({
+  mixins: [ Router.State ],
+
+  render: function () {
+    var age = this.getQuery().showAge ? '33' : '';
+    var userID = this.getParams().userID;
+    return (
+      <div className="User">
+        <h1>User id: {userID}</h1>
+        {age}
+      </div>
+    );
+  }
+});
+
+var routes = (
+  <Route handler={App}>
+    <Route name="user" path="user/:userID" handler={User}/>
+  </Route>
+);
+
+Router.run(routes, function (Handler) {
+  React.render(<Handler/>, document.getElementById('example'));
+});
